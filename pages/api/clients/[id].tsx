7e8aef4c541b514addc35aaa65f807bb0131126d
alcantaraDev <alcantara.isaque167@gmail.com>
2023-03-22 21:41:17 -0300

    update: get client by id

--- pages/api/clients/[id].tsx
+++ pages/api/clients/[id].tsx
@@ -5,8 +5,8 @@ export default async function handler(req:NextApiRequest, res:NextApiResponse) {
     switch (req.method) {
         case "GET":
             const clients = await getClient(req.query)
-            if (clients) res.status(200).json(clients)
-            else res.status(404).json(clients)
+            if (clients) res.status(200).json(clients[0])
+            else res.status(404).json("")
             break;
         default:
             res.status(404).json("")
