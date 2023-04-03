94d3722aed92b8ad7543e4b0eb7751aa07734876
alcantaraDev <alcantara.isaque167@gmail.com>
2023-04-03 13:01:52 -0300

    Revert "className prop updated"
    This reverts commit 44b088e456a3968248bdc342099779d23ce01d68.

--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -3,7 +3,6 @@ import {Poppins} from "next/font/google"
 import { PageLayout } from "@/components/pageLayote"
 import { TitleBar } from "@/components/titleBar"
 import { useAuth } from "@/contexts/auth"
-import { Cart } from "@/components/cart"
 
 const poppins = Poppins({
     weight: ["100","200", '300', "400", "500", "600", "700", "800", "900"],
@@ -17,7 +16,6 @@ export default function Page() {
         <div style={poppins.style}>
             <PageLayout>
                 <TitleBar title="caixa"/>
-                <Cart/>
             </PageLayout>
         </div>
     )
