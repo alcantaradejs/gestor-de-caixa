a57676093f5c543e1606a444f0a748b3bb75f37a
alcantaraDev <alcantara.isaque167@gmail.com>
2023-04-03 12:55:28 -0300

    basic layout implemented

--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -1,11 +1,22 @@
+import {Poppins} from "next/font/google"
+
+import { PageLayout } from "@/components/pageLayote"
+import { TitleBar } from "@/components/titleBar"
 import { useAuth } from "@/contexts/auth"
+
+const poppins = Poppins({
+    weight: ["100","200", '300', "400", "500", "600", "700", "800", "900"],
+    subsets: ["latin"]
+}) 
 
 export default function Page() {
     const {user, signOut} = useAuth()
     
     return (
-        <div>
-            Ola! {user?.name}
+        <div style={poppins.style}>
+            <PageLayout>
+                <TitleBar title="caixa"/>
+            </PageLayout>
         </div>
     )
 }
